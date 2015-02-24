4068ef625fb67bdb2111ae7f1efc16c98453880f
torchedplatypi <torchedplatypi@gmail.com>
2015-02-24 02:24:25 -0500

    Finalized version of P1-2

--- P1-2.asm
+++ P1-2.asm
@@ -14,12 +14,14 @@ MatchChecks:	.word  0,0
 
 .text
 IconMatch:	addi	$1, $0, CandBase	# point to base of Candidates
+#		addi	$2,$0,-1
 		swi	584			# generate puzzle icons
 
 		# your code goes here
 		#initialize variables i'll need
-		addi	$3,$0,528 #start index for finding color
+		addi	$3,$0,524 #start index for finding color
 		addi	$8,$0,576
+		addi	$1,$0,1
 
 		#initial loop for colored pixel.
 FindColorLoop:	addi	$3,$3,-4		#decrement index
@@ -28,24 +30,23 @@ FindColorLoop:	addi	$3,$3,-4		#decrement index
 		
 		#once a pixel of color is found, I'll be here.
 		addi	$5,$0,7			#set candidate index
+		add	$27,$0,$0
 ColorFound:	mult	$5,$8			#calculate index
 		mflo	$6			#per candidate
 		add	$6,$6,$3		#based on 7-0 index
 		lw	$7,CandBase($6)		#load candidate pix val
 		beq	$7,$4,PixelMatch
 JumpBack:	addi	$5,$5,-1		#decrement index
-		slt	$27,$5,$0#REPLACE 27 WITH 2 AFTER WRITING STORAGE
-		bne	$27,$0, IconsChecked#REPLACE 27 WITH 2 AFTER STORAGE
+		slt	$26,$5,$0#REPLACE 27 WITH 2 AFTER WRITING STORAGE
+		bne	$26,$0, IconsChecked#REPLACE 27 WITH 2 AFTER STORAGE
 		j	ColorFound
 
-PixelMatch:	sb	$4,MatchChecks($5)
+PixelMatch:	sb	$1,MatchChecks($5)
 		add	$2,$5,$0
+		add	$27,$27,$1
 		j	JumpBack
 
-IconsChecked:	addi	$27,$0,0
-
-
-
-                #addi    $2, $0, 0               # temp: guess the first icon
+IconsChecked:	bne	$27,$1,FindColorLoop 
+		#addi    $2, $0, 0               # temp: guess the first icon
 		swi	544			# submit answer and check
 		jr	$31			# return to caller
