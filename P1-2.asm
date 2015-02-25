ead381f5a44b39bab19269e3f712945cb9f161c0
Alex Moore <torchedplatypi@gmail.com>
2015-02-25 15:21:56 -0500

    comments and removed some unnecessary storage code

--- P1-2.asm
+++ P1-2.asm
@@ -10,19 +10,17 @@
 .data
 CandBase: 	.alloc 1152
 PatternBase:	.alloc 144
-MatchChecks:	.word  0,0
 
 .text
 IconMatch:	addi	$1, $0, CandBase	# point to base of Candidates
-#		addi	$2,$0,-1
+#		addi	$2,$0,-1		#for testing
 		swi	584			# generate puzzle icons
 
 		# your code goes here
 		#initialize variables i'll need
 		addi	$3,$0,524 #start index for finding color
-		addi	$8,$0,576
-		addi	$1,$0,1
-
+		addi	$8,$0,576 #need a constant 144*4 for mult later
+		addi	$1,$0,1   #need a constant 1 for later
 		#initial loop for colored pixel.
 FindColorLoop:	addi	$3,$3,-4		#decrement index
 		lw	$4,PatternBase($3)	#load pixel value
@@ -35,18 +33,19 @@ ColorFound:	mult	$5,$8			#calculate index
 		mflo	$6			#per candidate
 		add	$6,$6,$3		#based on 7-0 index
 		lw	$7,CandBase($6)		#load candidate pix val
-		beq	$7,$4,PixelMatch
+		beq	$7,$4,PixelMatch	#if match, note and react
 JumpBack:	addi	$5,$5,-1		#decrement index
-		slt	$26,$5,$0#REPLACE 27 WITH 2 AFTER WRITING STORAGE
-		bne	$26,$0, IconsChecked#REPLACE 27 WITH 2 AFTER STORAGE
-		j	ColorFound
+		slt	$26,$5,$0		#check if thru all 8 checks
+		bne	$26,$0, IconsChecked	#if so, continue to next sec.
+		j	ColorFound		#loop if not
 
-PixelMatch:	sb	$1,MatchChecks($5)
-		add	$2,$5,$0
-		add	$27,$27,$1
-		j	JumpBack
+PixelMatch:	add	$2,$5,$0	#w/o interrupting parent loop, this part
+		addi	$27,$27,1	#stores match candidate. and num matches
+		j	JumpBack	#If num = 1, at end, then stored val is
+					#the final answer
 
-IconsChecked:	bne	$27,$1,FindColorLoop 
+IconsChecked:	bne	$27,$1,FindColorLoop #if more than one match, restart
+					#find a unique pixel instead.
 		#addi    $2, $0, 0               # temp: guess the first icon
 		swi	544			# submit answer and check
 		jr	$31			# return to caller
